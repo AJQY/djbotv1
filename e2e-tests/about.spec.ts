35cd6b0ad005a79ccad38cc3f4f5d4baceb5f8c4
AJQY <107565299+AJQY@users.noreply.github.com>
2020-12-30 12:20:27 +0000

    Update main.h

--- e2e-tests/about.spec.ts
+++ e2e-tests/about.spec.ts
@@ -3,4 +3,6 @@ describe('about modal', function () {
         browser.get('http://localhost:' + (process.env.PORT || 8000));
     });
 
-    it('opens a modal', function (
+    it('opens a modal', function () {
+        var openButton = element(by.css('button[aria-label=About]'));
+        openButton.click()
