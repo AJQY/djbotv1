c9017753b9952a8fdb14cceacf0600beb441d042
AJQY <107565299+AJQY@users.noreply.github.com>
2021-01-01 22:03:54 +0000

    Fixes and cleanup

--- e2e-tests/login.spec.ts
+++ e2e-tests/login.spec.ts
@@ -1,3 +1,6 @@
 describe('authentication interface', function () {
     var dialog = element(by.tagName('md-dialog'));
-    var buttons = element(by.tagName('md-dialog-
+    var buttons = element(by.tagName('md-dialog-actions')).all(by.tagName('button'));
+
+    beforeEach(function () {
+ 
