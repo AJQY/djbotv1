02a763e8282833582031f3073f16725d53968fbe
AJQY <107565299+AJQY@users.noreply.github.com>
2021-01-02 08:10:28 +0000

    merging git tree

--- e2e-tests/login.spec.ts
+++ e2e-tests/login.spec.ts
@@ -3,4 +3,7 @@ describe('authentication interface', function () {
     var buttons = element(by.tagName('md-dialog-actions')).all(by.tagName('button'));
 
     beforeEach(function () {
- 
+        browser.get('http://localhost:' + (process.env.PORT || 8000));
+    });
+
+   
