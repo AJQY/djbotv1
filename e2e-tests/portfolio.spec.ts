c1af47269afa5c2b09fdfcb86c3aeaf57c973457
AJQY <107565299+AJQY@users.noreply.github.com>
2021-01-19 20:57:15 +0000

    fix windows build

--- e2e-tests/portfolio.spec.ts
+++ e2e-tests/portfolio.spec.ts
@@ -1,4 +1,7 @@
 describe('portfolio interface', function () {
     beforeEach(function () {
         browser.get('http://localhost:' + (process.env.PORT || 8000) + '/#/tabs/portfolio');
-        //element.all(by.tagName('md-tab-item')).get
+        //element.all(by.tagName('md-tab-item')).get(1).click();
+    });
+
+    it('runs through the basic stock functions', fun
