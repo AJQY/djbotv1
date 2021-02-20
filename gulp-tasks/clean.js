3156ec259439f5896f7e4f594a74f9d73f2d7f8e
AJQY <107565299+AJQY@users.noreply.github.com>
2021-02-20 11:21:16 +0000

    Fixing test case

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -3,4 +3,7 @@ var gulp = require('gulp');
 var del = require('del');
 var g = require('./global.json');
 
-/
+/* Clean tasks */
+
+//Clean everything
+gulp.task('clean', ['clean-npm', 'clean-build', 'clean-typings',
