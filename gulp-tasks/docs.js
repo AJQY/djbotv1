0c5e22d3778193c209e1d11c335ed82d3140c7cc
AJQY <107565299+AJQY@users.noreply.github.com>
2021-03-03 09:10:29 +0000

    Fix animations

--- gulp-tasks/docs.js
+++ gulp-tasks/docs.js
@@ -3,4 +3,7 @@ var ngDocs = require('gulp-ngdocs');
 var g = require('./global.json');
 
 gulp.task('docs', ['concat'], function () {
-  return gulp.
+  return gulp.src(g.SRC + '**/*.js')
+    .pipe(ngDocs.process({
+      loadDefaults: {
+ 
