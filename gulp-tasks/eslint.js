a614781c62302b699ed5b0acbafb951ce32ef7ca
AJQY <107565299+AJQY@users.noreply.github.com>
2021-03-07 18:34:29 +0000

    update jslib

--- gulp-tasks/eslint.js
+++ gulp-tasks/eslint.js
@@ -4,4 +4,7 @@ var g = require('./global.json');
 
 gulp.task('lint', function () {
     return gulp
-       
+        .src([g.SRC + '**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(
