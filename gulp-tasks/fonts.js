bcd69506485e83544e540cc788c7f0b9e652aa66
AJQY <107565299+AJQY@users.noreply.github.com>
2021-03-08 07:06:26 +0000

    chore: add changelog

--- gulp-tasks/fonts.js
+++ gulp-tasks/fonts.js
@@ -1,2 +1,7 @@
 var gulp = require('gulp');
-var googleWebFonts = require('gulp-googl
+var googleWebFonts = require('gulp-google-webfonts');
+var g = require('./global.json');
+
+gulp.task('fonts', function () {
+  return gulp
+   
