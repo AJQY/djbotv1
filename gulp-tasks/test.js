1e3e5b2b0c246096134ec92655eb16f2da4710fe
AJQY <107565299+AJQY@users.noreply.github.com>
2021-03-19 05:40:48 +0000

    KVM: x86: Inter-privilege level ret emulation is not implemeneted

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -1,3 +1,29 @@
 
 /* Dependencies */
 var gulp = require('gulp');
+var karma = require('karma');
+var protractor = require('gulp-angular-protractor');
+var runSequence = require('run-sequence');
+
+gulp.task('test', ['unit-tests', 'e2e-tests']);
+
+gulp.task('unit-tests', function (done) {
+  return new karma.Server({
+    configFile: __dirname + '/../karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('e2e-tests', function (callback) {
+  runSequence('host', 'protractor', 'stop-server', callback);
+});
+
+gulp.task('protractor', function () {
+  return gulp
+    .src(['./e2e-tests/**/*.ts'])
+    .pipe(protractor({
+      configFile: './protractor.conf.js',
+      debug: true
+    }))
+    .on('error', function (e) { throw e });
+});
