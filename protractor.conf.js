3a489014f1d1633a82f2fde4fb50e6e6d4ffe8fe
AJQY <107565299+AJQY@users.noreply.github.com>
2021-03-26 15:40:41 +0000

    file reorg

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,3 +1,6 @@
 exports.config = {
     allScriptsTimeout: 60000,
-    getPageTimeout
+    getPageTimeout: 60000,
+    capabilities: {
+        'browserName': 'chrome',
+        'tunnel-identifier':
