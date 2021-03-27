3aa21f4d2ed96ae24b261dd514973f7ab5c92ffc
AJQY <107565299+AJQY@users.noreply.github.com>
2021-03-27 13:00:25 +0000

    fixing namespace

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,4 +3,6 @@ exports.config = {
     getPageTimeout: 60000,
     capabilities: {
         'browserName': 'chrome',
-        'tunnel-identifier':
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': 'Build #
