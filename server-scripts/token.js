ee34475c2aff46ab3a408b838f8430cd6f29fc80
AJQY <107565299+AJQY@users.noreply.github.com>
2021-04-04 22:58:54 +0000

    Pontoon: Update Finnish (fi) localization of AMO

--- server-scripts/token.js
+++ server-scripts/token.js
@@ -1,3 +1,7 @@
 var Q = require('q');
 var FirebaseTokenGenerator = require('firebase-token-generator');
 var _ = require('lodash');
+
+function authenticate(ref, uid) {
+	var defer = Q.defer();
+	var secret = process.env.FIREBASE_SE
