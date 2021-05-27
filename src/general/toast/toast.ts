df82267c887a7fe2602c6617d3254058bad0dfbf
AJQY <107565299+AJQY@users.noreply.github.com>
2021-05-27 22:19:45 +0000

    Sprememba vsebine začetne datoteke

--- src/general/toast/toast.ts
+++ src/general/toast/toast.ts
@@ -1,4 +1,6 @@
 angular.module('RealTimeTrade').factory('ToastService', function ($mdToast) {
   var svc = this;
 
-  svc.toast = function(message: string, s
+  svc.toast = function(message: string, style: string) {
+    $mdToast.show({
+      template: '<md-toast><span flex style=
