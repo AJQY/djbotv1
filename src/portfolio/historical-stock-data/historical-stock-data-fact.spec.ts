e63456e24dca507d96955c370345ba14b14d705e
AJQY <107565299+AJQY@users.noreply.github.com>
2021-12-04 15:20:21 +0000

    checking

--- src/portfolio/historical-stock-data/historical-stock-data-fact.spec.ts
+++ src/portfolio/historical-stock-data/historical-stock-data-fact.spec.ts
@@ -6,4 +6,7 @@ describe('HistoricalStockDataService', function () {
     });
 
     beforeEach(inject(function (HistoricalStockDataService, _$q_, _$rootScope_, _$httpBackend_) {
-        svc = HistoricalSto
+        svc = HistoricalStockDataService;
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+        $httpBackend =
