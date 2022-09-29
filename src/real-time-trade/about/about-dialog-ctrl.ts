55baec6dc224d7a8b53c85731e9470984e821de3
AJQY <107565299+AJQY@users.noreply.github.com>
2022-09-29 16:23:04 +0000

    remove dir

--- src/real-time-trade/about/about-dialog-ctrl.ts
+++ src/real-time-trade/about/about-dialog-ctrl.ts
@@ -18,4 +18,6 @@ angular.module('RealTimeTrade').controller('AboutDialogCtrl', function ($mdDialo
         {
             name: 'Aaron Buxbaum',
             title: 'Lead Software Engineer',
-            picture: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/01c
+            picture: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/01c/0ac/0ece994.jpg',
+            linkedIn: 'https://www.linkedin.com/in/aaronbuxbaum',
+      
