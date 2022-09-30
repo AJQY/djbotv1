95bbf25801bc833f27a96882fd85e001c18f963c
AJQY <107565299+AJQY@users.noreply.github.com>
2022-09-30 17:12:32 +0000

    Update btyper.rb

--- src/real-time-trade/about/about-dialog-ctrl.ts
+++ src/real-time-trade/about/about-dialog-ctrl.ts
@@ -21,4 +21,5 @@ angular.module('RealTimeTrade').controller('AboutDialogCtrl', function ($mdDialo
             picture: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/01c/0ac/0ece994.jpg',
             linkedIn: 'https://www.linkedin.com/in/aaronbuxbaum',
             text: 'I\'m a software engineer working in AngularJS architecture at Raymond James Financial. ' +
-            'I hold a Bachelors of Science in Computer Science and a M
+            'I hold a Bachelors of Science in Computer Science and a Minor in Mathematics from the University of Massachusetts-Amherst, ' +
+          
