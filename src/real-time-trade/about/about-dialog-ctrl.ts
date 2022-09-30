8884e515a6c6650f60da1df0ba705a63e3f3a573
AJQY <107565299+AJQY@users.noreply.github.com>
2022-09-30 10:07:34 +0000

    clean the code

--- src/real-time-trade/about/about-dialog-ctrl.ts
+++ src/real-time-trade/about/about-dialog-ctrl.ts
@@ -20,4 +20,5 @@ angular.module('RealTimeTrade').controller('AboutDialogCtrl', function ($mdDialo
             title: 'Lead Software Engineer',
             picture: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/01c/0ac/0ece994.jpg',
             linkedIn: 'https://www.linkedin.com/in/aaronbuxbaum',
-            text: 'I\'m a software engineer working in AngularJS architecture at Raymond James Fi
+            text: 'I\'m a software engineer working in AngularJS architecture at Raymond James Financial. ' +
+            'I hold a Bachelors of Science in Computer Science and a M
