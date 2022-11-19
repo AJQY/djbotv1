06b3e8b806b4ad01f513a35119a1d2509042a33a
AJQY <107565299+AJQY@users.noreply.github.com>
2022-11-19 20:22:25 +0000

    resize window

--- src/real-time-trade/login/login.spec.ts
+++ src/real-time-trade/login/login.spec.ts
@@ -3,4 +3,7 @@ describe('Login', function () {
 
     beforeEach(module('RealTimeTrade', 'RealTimeTrade.Templates'));
 
-    beforeEach(inject(function ($compile, $rootScope, _$m
+    beforeEach(inject(function ($compile, $rootScope, _$mdDialog_) {
+        $scope = $rootScope.$new();
+        $mdDialog = _$mdDialog_;
+   
