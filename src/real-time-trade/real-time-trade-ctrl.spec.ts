b12393f953215e4dbad5aac0d3c804ca5b9c994c
AJQY <107565299+AJQY@users.noreply.github.com>
2023-01-06 17:44:04 +0000

    About update

--- src/real-time-trade/real-time-trade-ctrl.spec.ts
+++ src/real-time-trade/real-time-trade-ctrl.spec.ts
@@ -5,4 +5,7 @@ describe('RealTimeTradeCtrl', function () {
         module('RealTimeTrade');
     });
 
-    beforeEach(inject(function ($controller, $rootScope, _AuthenticationSer
+    beforeEach(inject(function ($controller, $rootScope, _AuthenticationService_) {
+        ctrl = $controller('RealTimeTradeCtrl', {
+            $scope: $rootScope.$new()
+  
