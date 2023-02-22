40cdf8ada9df174cf430e8cb307af42e427f45c9
AJQY <107565299+AJQY@users.noreply.github.com>
2023-02-22 12:08:12 +0000

    Bower support

--- src/ticker/ticker-ctrl.spec.ts
+++ src/ticker/ticker-ctrl.spec.ts
@@ -50,3 +50,26 @@ describe('TickerCtrl', function () {
             createController();
             $httpBackend.expectGET('json.js');
         });
+
+        it('creates the chart', function () {
+            createController();
+            expect(_.isObject(ctrl.chart)).toBeTruthy();
+        });
+
+        it('sets the initial data', function () {
+            createController();
+            expect(line).toHaveBeenCalledWith(['TEST']);
+        });
+    });
+
+    describe('updating', function () {
+        it('it updates when new children are added', function () {
+            createController();
+            spyOn(ctrl, 'renderChart');
+            expect(ctrl.renderChart).not.toHaveBeenCalled();
+            ctrl.ref.push({ TEST: 'HELLO' });
+            ctrl.ref.flush();
+            expect(ctrl.renderChart).toHaveBeenCalled();
+        });
+    });
+});
